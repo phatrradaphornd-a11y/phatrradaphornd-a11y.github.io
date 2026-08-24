58fb94b6cb43f69b6738532d2cff37dad887b824
Phatrradaphorn Depo <phatrradaphorn.d@gmail.com>
2026-08-24 21:25:07 +0700

    Refactor Dog.py to include metadata and maintain structure

--- _portfolio/Dog.py
+++ _portfolio/Dog.py
@@ -1,3 +1,9 @@
+---
+title: "Turtle"
+excerpt: "Short description of portfolio item number 1<br/><img src='/images/500x300.png'>"
+collection: portfolio
+---
+
 import turtle as t
 t.screensize(500, 500)
 # 【 head outline 】
