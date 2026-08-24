e8fedc684a7486d5a921ad561fe3bd61221315e7
Phatrradaphorn Depo <phatrradaphorn.d@gmail.com>
2026-08-24 21:28:06 +0700

    Modify portfolio item details in Dog.py
    Updated portfolio item description and image.

--- _portfolio/Dog.py
+++ _portfolio/Dog.py
@@ -1,6 +1,6 @@
 ---
 title: "Turtle"
-excerpt: "Short description of portfolio item number 1<br/><img src='/images/500x300.png'>"
+excerpt: "Short description of portfolio item number 1<br/><img src='/images/Screenshot 2026-08-17 174146.png'>"
 collection: portfolio
 ---
 
